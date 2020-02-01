e723cf4c8a82ff1b068f3e6cc2363aaafbcef90e
math <mathilde.grapin@gmail.com>
2020-02-01 17:10:23 +0100

    :tada: Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld :msg="name"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -13,9 +13,6 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  },
-  data: () => ({
-    name: 'Salut'
-  })
+  }
 }
 </script>
